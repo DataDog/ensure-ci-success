df79d7656fddcf53d8b4481244e0c9c5e0d4643b
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2025-04-28 17:27:06 +0200

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -34523,6 +34523,8 @@ async function run() {
     }
 
     const sha = pr.head.sha;
+    const currentWorkflow = github.context.workflow;
+    const currentJob = github.context.job;
 
     core.info(`Sleeping for ${initialDelaySeconds} seconds before starting checks...`);
     await sleep(initialDelaySeconds);
@@ -34552,6 +34554,10 @@ async function run() {
 
       // Analyze check runs
       for (const check of checkRuns) {
+        if (check.name === currentWorkflow || check.name === currentJob) {
+          core.info(`Skipping current running check: ${check.name}`);
+          continue;  // Skip our own job
+        }
         if (check.status === 'queued' || check.status === 'in_progress') {
           stillRunning = true;
         } else if (check.conclusion !== 'success' && check.conclusion !== 'skipped') {
